55ee50806b5a7669eaa3bc433d48aa199676d8af
Samantha Harlin <samscomputer@Samanthas-MBP-2.cfl.rr.com>
2024-12-18 10:42:46 -0500

    remove cluster.yaml

--- guestbook/argocd-manager-sa.sh
+++ guestbook/argocd-manager-sa.sh
@@ -4,3 +4,5 @@ kubectl create serviceaccount argocd-manager -n kube-system
 kubectl create clusterrolebinding argocd-manager-role --clusterrole=cluster-admin --serviceaccount=kube-system:argocd-manager
 
 kubectl apply -f argocd-manager-token.yaml
+
+# pull secret from argocd secret token in -n kube-system
